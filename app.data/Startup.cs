f40bcdda6731d5061353403dbdd02b18689f1e19
tanok78 <97112247+tanok78@users.noreply.github.com>
2022-01-05 18:45:27 +0300

    Update Startup.cs

--- app.data/Startup.cs
+++ app.data/Startup.cs
@@ -51,7 +51,7 @@ namespace app.data
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            // comments for test
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
