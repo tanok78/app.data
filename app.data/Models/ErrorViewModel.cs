e145ddbc484ad6ec4da96ec13e3da6d8f166dcd0
tanok78 <97112247+tanok78@users.noreply.github.com>
2022-01-05 18:42:01 +0300

    Add files via upload

---
+++ app.data/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace app.data.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
